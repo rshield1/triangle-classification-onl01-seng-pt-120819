9801bba23ee8ff1a5db3377bdd3c13f5acb4f52c
Robert Shields <robert.shieldsiv@gmail.com>
2020-01-14 20:58:08 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,15 +1,21 @@
 class Triangle
- attr_accessor :equilateral, :isosceles, :scalene
+ attr_accessor :side_1, :side_2 :side_3
  
- def initialize(arg1,arg2,arg3)
-   
+ def initialize(side_1,side_2,side_3)
+   @side_1 = side_1
+   @side_2 = side_2
+   @side_3 = side_3
  end
  
  def kind
-   @kind
+   
+   
+   return type should be a symbol
  end
  
  
- 
+ class TriangleError < StandardError
+   
+ end
  
 end
