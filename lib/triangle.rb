eea9fed5afc7d22f2ba5ea20c03ec0a6c100f1c3
Robert Shields <robert.shieldsiv@gmail.com>
2020-01-14 21:03:48 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -17,6 +17,14 @@ class Triangle
       :scalene
     end
   end
+  
+  def validate_triangle
+    real_triangle = [(side_1 + side_2 > side_3), (side_1 + side_3 > side_2), (side_2 + side_3 > side_1)]
+    [side_1, side_2, side_3].each do |side|
+      real_triangle << false if side <= 0 
+    raise TriangleError if real_triangle.include?(false)
+    end
+  end
  
  
  class TriangleError < StandardError
